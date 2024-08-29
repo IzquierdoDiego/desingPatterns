ee18e937f31a545bcf2e75c56145473269257672
Diego Izquierdo <izquierdodiegof@gmail.com>
2024-08-28 20:59:09 -0500

    Builder, with Director, and tow products

---
+++ Builder2/src/main/java/org/example/builders/Builder.java
@@ -0,0 +1,14 @@
+package org.example.builders;
+
+import org.example.domain.*;
+
+public interface Builder {
+
+    void setCarType(CarType type);
+    void setSeats(int seats);
+    void setEngine(Engine engine);
+    void setTransmission(Transmission transmission);
+    void setTripComputer(TripComputer tripComputer);
+    void setGPSNavigator(GPSNavigator gpsNavigator);
+
+}
---
+++ Builder2/src/main/java/org/example/domain/Transmission.java
@@ -0,0 +1,5 @@
+package org.example.domain;
+
+public enum  Transmission {
+    SINGLE_SPEED, MANUAL, AUTOMATIC, SEMI_AUTOMATIC
+}
